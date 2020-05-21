1090627ed7d869a8be1c08240d7192c798681233
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-21 11:56:28 -0500

    Search and clear functioning!

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import "./App.scss";
 import axios from "axios";
+import Search from "./components/users/Search";
 
 class App extends React.Component {
   state = {
     users: [],
-    loading: false
-  }
+    loading: false,
+  };
 
-  async componentDidMount() {
-    this.setState({ loading: true })
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`);
-    this.setState({ users: res.data, loading: false})
+  searchUsers = async (text) => {
+    this.setState({loading: true})
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers= () => {
+    this.setState({loading: true})
+    this.setState({users: [], loading: false})
   }
 
   render() {
@@ -21,7 +29,8 @@ class App extends React.Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users}/>
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true: false}/>
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
